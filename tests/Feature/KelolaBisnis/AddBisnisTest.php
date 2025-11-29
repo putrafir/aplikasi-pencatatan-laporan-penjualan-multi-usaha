1c20662af865c93f1acce59364c457dc09b10bb9
putrafir <firdausputra0212@gmail.com>
2025-11-29 17:27:07 +0700

    test

--- tests/Feature/KelolaBisnis/AddBisnisTest.php
+++ tests/Feature/KelolaBisnis/AddBisnisTest.php
@@ -1,7 +1,20 @@
 <?php
 
-test('example', function () {
-    $response = $this->get('/');
+test('dapat menambah bisnis baru', function () {
+    $response = $this->withoutMiddleware()->post(route('admin.kelola-bisnis.add'), [
+        'name' => 'Toko Pisang Goreng',
+        'lokasi' => 'Jalan Mawar No. 10',
+    ]);
 
-    $response->assertStatus(200);
+    // Pastikan redirect sukses
+    $response->assertRedirect();
+
+    // Pastikan pesan sukses ada di session
+    $response->assertSessionHas('success');
+
+    // Pastikan data masuk database
+    $this->assertDatabaseHas('business', [
+        'name' => 'Toko Pisang Goreng',
+        'lokasi' => 'Jalan Mawar No. 10',
+    ]);
 });
